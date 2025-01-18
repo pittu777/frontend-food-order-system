819880f805e69f322228c4a36de2036b638c635a
pittu777 <pittuprasanth98@gmail.com>
2025-01-18 17:28:53 +0530

    updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -24,6 +24,7 @@ const Login = () => {
  
   
   return (
+    <>
     <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
       <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
       {error && <p className="text-red-500">{error}</p>}
@@ -33,18 +34,21 @@ const Login = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         className="w-full p-2 mb-4 border rounded"
-      />
+        />
       <input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         className="w-full p-2 mb-4 border rounded"
-      />
+        />
       <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded w-full">
         Login
       </button>
+     
     </form>
+   <a href="/admin-login">admin?</a>
+        </>
   );
 };
 
